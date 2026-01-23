7ba42f03e895c2708ddbce66c2c0dd54c8a2b5f6
TVis2010 <tara.viswanath2010@gmail.com>
2026-01-23 16:59:54 -0500

    12302

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative25812.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative25812.java
@@ -112,8 +112,10 @@ public class BasicOpMode_Iterative25812 extends OpMode
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double backLeftPower;
+        double backRightPower;
+        double frontLeftPower;
+        double frontRightPower;
 
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
@@ -122,8 +124,8 @@ public class BasicOpMode_Iterative25812 extends OpMode
         // - This uses basic math to combine motions and is easier to drive straight.
         double drive = -gamepad1.left_stick_y;
         double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        //leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        //rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
@@ -136,7 +138,7 @@ public class BasicOpMode_Iterative25812 extends OpMode
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        //telemetry.addData("Motors", "left (%.2f), right (%.2f)",//leftPower, rightPower);
     }
 
     /*
